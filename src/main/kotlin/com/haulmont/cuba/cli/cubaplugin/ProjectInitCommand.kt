259723d77d4b9ef95e29a01a3e97403536c89152
Alexander Shustanov <shustanov@haulmont.com>
2018-05-07 10:54:21 +0400

    Different database types in project template #20

--- src/main/kotlin/com/haulmont/cuba/cli/cubaplugin/ProjectInitCommand.kt
+++ src/main/kotlin/com/haulmont/cuba/cli/cubaplugin/ProjectInitCommand.kt
@@ -17,7 +17,9 @@
 package com.haulmont.cuba.cli.cubaplugin
 
 import com.beust.jcommander.Parameters
+import com.haulmont.cuba.cli.commands.CommandExecutionException
 import com.haulmont.cuba.cli.commands.GeneratorCommand
+import com.haulmont.cuba.cli.commands.from
 import com.haulmont.cuba.cli.commands.nameFrom
 import com.haulmont.cuba.cli.generation.TemplateProcessor
 import com.haulmont.cuba.cli.kodein
@@ -82,6 +84,10 @@ class ProjectInitCommand : GeneratorCommand<ProjectInitModel>() {
         options("platformVersion", "Platform version", availablePlatformVersions) {
             default(0)
         }
+
+        options("database", "Choose database", availableDatabases) {
+            default(0)
+        }
     }
 
     override fun createModel(answers: Answers): ProjectInitModel = ProjectInitModel(answers)
@@ -106,6 +112,8 @@ class ProjectInitCommand : GeneratorCommand<ProjectInitModel>() {
             }
         }
 
+//        TODO move texts to resources
+
         writer.println("""
 
             @|white Project generated at ${cwd.toAbsolutePath()} directory
@@ -113,6 +121,36 @@ class ProjectInitCommand : GeneratorCommand<ProjectInitModel>() {
             To import project into IDE execute `gradlew ide` and open generated *.iml file|@
 
         """.trimIndent())
+
+        val model = context.getModel<ProjectInitModel>("project")
+        when (model.database.database) {
+//            oracle
+            availableDatabases[5] -> writer.println("""
+            @|white You have selected Oracle Database.
+
+            Oracle does not allow to redistribute its JDBC driver, so do the following:
+            > Download ojdbc6.jar from www.oracle.com.
+            > Run Deploy from the main menu, then copy ojdbc6.jar into {your_project}/deploy/tomcat/lib directory.
+            > Copy ojdbc6.jar into ${'$'}{user.home}/.haulmont/studio/lib directory.
+            > Stop Studio server (press Exit button).
+            > Kill Gradle daemon by running gradle --stop, or killing its process in task manager, or restarting the operating system.
+            > Launch Studio server again.|@
+
+        """.trimIndent())
+//            mysql
+            availableDatabases[6] -> writer.println("""
+            @|white You have selected MySQL database.
+
+            MySQL does not allow to redistribute its JDBC driver, so do the following:
+            > Download MySQL Connector/J archive from dev.mysql.com/downloads/connector/j.
+            > Extract mysql-connector-java-*.jar file from the archive and rename it to mysql-connector-java.jar
+            > Copy mysql-connector-java.jar into ${'$'}{user.home}/.haulmont/studio/lib directory.
+            > Stop Studio server (press Exit button).
+            > Kill Gradle daemon by running gradle --stop, or killing its process in task manager, or restarting the operating system.
+            > Launch Studio server again.|@
+
+        """.trimIndent())
+        }
     }
 }
 
@@ -122,7 +160,66 @@ class ProjectInitModel(answers: Answers) {
     val rootPackage: String by nameFrom(answers)
     val platformVersion: String by nameFrom(answers)
     val rootPackageDirectory: String = rootPackage.replace('.', File.separatorChar)
+    val database: DatabaseModel = DatabaseModel("database" from answers)
+}
 
+class DatabaseModel(val database: String) {
+    val schema: String
+    val driver: String
+    val driverDependency: String
+    val driverDependencyName: String
+    val connectionParams: String = if (database == availableDatabases.last()) {
+        "?useSSL=false&amp;allowMultiQueries=true"
+    } else ""
+
+    init {
+        when (database) {
+            availableDatabases[0] -> {
+                schema = "jdbc:hsqldb:hsql:"
+                driver = "org.hsqldb.jdbc.JDBCDriver"
+                driverDependency = "\"org.hsqldb:hsqldb:2.2.9\""
+                driverDependencyName = "hsql"
+            }
+            availableDatabases[1] -> {
+                schema = "jdbc:postgresql:"
+                driver = "org.postgresql.Driver"
+                driverDependency = "\"org.postgresql:postgresql:9.4.1212\""
+                driverDependencyName = "postgres"
+            }
+            availableDatabases[2] -> {
+                schema = "jdbc:sqlserver:"
+                driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+                driverDependency = "\"com.microsoft.sqlserver:mssql-jdbc:6.2.1.jre8\""
+                driverDependencyName = "mssql"
+            }
+            availableDatabases[3] -> {
+                schema = "jdbc:jtds:sqlserver:"
+                driver = "net.sourceforge.jtds.jdbc.Driver"
+                driverDependency = "\"net.sourceforge.jtds:jtds:1.3.1\""
+                driverDependencyName = "mssql"
+            }
+            availableDatabases[4] -> {
+                schema = "jdbc:sqlserver:"
+                driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+                driverDependency = "\"com.microsoft.sqlserver:mssql-jdbc:6.2.1.jre8\""
+                driverDependencyName = "mssql"
+            }
+            availableDatabases[5] -> {
+                schema = "jdbc:oracle:thin:@"
+                driver = "oracle.jdbc.OracleDriver"
+                driverDependency = "files(\"\$cuba.tomcat.dir/lib/ojdbc6.jar\")"
+                driverDependencyName = "oracle"
+            }
+            availableDatabases[6] -> {
+                schema = "jdbc:mysql:"
+                driver = "com.mysql.jdbc.Driver"
+                driverDependency = "\"mysql:mysql-connector-java:5.1.38\""
+                driverDependencyName = "mysql"
+            }
+            else -> throw CommandExecutionException("Unsupported database")
+        }
+    }
 }
 
 private val availablePlatformVersions = listOf("6.8.5", "6.9-SNAPSHOT")
+private val availableDatabases = listOf("HSQLDB", "PostgreSQL", "Microsoft SQL Server", "Microsoft SQL Server 2005", "Microsoft SQL Server 2012+", "Oracle Database", "MySQL")
