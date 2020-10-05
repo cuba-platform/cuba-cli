fc6824ff4424a6559c4d6d65485ef750a5dfa547
Alexander Shustanov <shustanov@haulmont.com>
2020-10-05 11:09:17 +0400

    Use Gradle 5.4.1 for projects based on CUBA 7.2 #184

---
+++ src/main/resources/com/haulmont/cuba/cli/cubaplugin/templates/project/7.2/modules/core/test/$[project.rootPackage]/core/SampleIntegrationTest.java
@@ -0,0 +1,53 @@
+package ${project.rootPackage}.core;
+
+import ${project.rootPackage}.AppTestContainer;
+import com.haulmont.cuba.core.EntityManager;
+import com.haulmont.cuba.core.Persistence;
+import com.haulmont.cuba.core.Transaction;
+import com.haulmont.cuba.core.TypedQuery;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.security.entity.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleIntegrationTest {
+
+    @ClassRule
+    public static AppTestContainer cont = AppTestContainer.Common.INSTANCE;
+
+    private Metadata metadata;
+    private Persistence persistence;
+    private DataManager dataManager;
+
+    @Before
+    public void setUp() throws Exception {
+        metadata = cont.metadata();
+        persistence = cont.persistence();
+        dataManager = AppBeans.get(DataManager.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testLoadUser() {
+        try (Transaction tx = persistence.createTransaction()) {
+            EntityManager em = persistence.getEntityManager();
+            TypedQuery<User> query = em.createQuery(
+                    "select u from sec$User u where u.login = :userLogin", User.class);
+            query.setParameter("userLogin", "admin");
+            List<User> users = query.getResultList();
+            tx.commit();
+            assertEquals(1, users.size());
+        }
+    }
+}
